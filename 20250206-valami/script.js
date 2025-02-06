4c8471bf92219358a761ce01198dbe939a08b8dc
Chlebik Krisztofer <susu@liba.lol>
2025-02-06 10:34:16 +0100

    tankcsapda
    Ülj le mellém
    Valamit mondok
    Szomjas vagy látom
    Egy üveg bort kibontok
    Figyelj
    Lehet, hogy nem vagy gyenge
    De ha a szívedbe szalad a penge
    Attól nem érzed magad jobban
    Ha a kocsidban bomba robban
    Tudom én, erős vagy persze
    De ha a fejedben ott van a fejsze
    Majd a fegyver csövébe nézel
    Ott már semmire nem mégy pénzzel és
    Hiába vagy gazdag
    Ha az égiek leszavaznak
    A kocka, ha el van vetve
    Te meg a föld alá temetve
    Ott már hiába van ügyvéd
    Aki a törvényektől megvéd
    Itt senki se golyóálló és
    És Ha szakad a védőháló
    A halálugrás végén a túlvilági TV-n majd
    Majd rólad szólnak a hírek
    Veled van tele a sajtó
    Aki a pokolra kíván jutni annak
    Balra a második ajtó
    De ha a Szent-Péter szigetekre már
    Be van fizetve az útja
    Önnek a Mennyország Tourist
    A legjobb szolgáltatást nyújtja
    És
    Lehet hogy nem vagy gyáva és
    A végén Te maradsz állva
    De mire jó úgy ez az élet
    Hogyha futnod kell, amíg éled
    És hiába vagy bátor
    Mint egy római gladiátor
    Aki keményebb mint a szikla
    Mégis lehet hogy elég egy szikra
    A gyújtózsinór végén és
    A túlvilági TV-n majd
    Majd rólad szólnak a hírek
    Veled van tele a sajtó
    Aki a pokolra kíván jutni annak
    Balra a második ajtó
    De ha a Szent-Péter szigetekre már
    Be van fizetve az útja
    Önnek a Mennyország Tourist
    A legjobb szolgáltatást nyújtja
    Mi Atyánk ki vagy a mennyekbe' mondd csak melyik ajtón menjek be?
    Mi Atyánk ki vagy a mennyekbe' mondd csak melyik ajtón menjek be?
    Mi Atyánk ki vagy a mennyekbe' mondd csak Én melyik ajtón menjek be?
    Mi atyánk ki vagy a mennyekbe mond csak!
    Az emberek meg néznek
    Hogy az Isten a pénz lett
    Sorban nyílnak a bankok és
    Az jelenti a rangot
    Hogy mennyire állat az autód
    Mekkora mellű a nőd és hogy
    Meddig bírod feltekerni
    A kocsiban a hangerőt
    A kocsiban a hangerőt!
    É-é-é
    Majd rólad szólnak a hírek
    Veled van tele a sajtó
    Aki a pokolra kíván jutni annak
    Balra a második ajtó
    De ha a Szent-Péter szigetekre már
    Be van fizetve az útja
    Önnek a Mennyország Tourist
    A legjobb szolgáltatást nyújtja
    Mi Atyánk ki vagy a mennyekbe' mondd csak melyik ajtón menjek be?
    Mi Atyánk ki vagy a mennyekbe' mondd csak melyik ajtón menjek be?
    Mi Atyánk ki vagy a mennyekbe' mondd csak melyik ajtón menjek be?
    Mi Atyánk ki vagy a mennyekbe' mondd csak melyik ajtón menjek be?
    Melyik ajtón menjek be?
    Én melyik ajtón menjek be?
    Melyik ajtón menjek be?
    Yeah
    Ülj le mellém
    Valamit mondok

---
+++ 20250206-valami/script.js
@@ -0,0 +1,45 @@
+function emailCimetGeneral(nev, ev) {
+    let output="";
+    nev.split(' ').forEach(el => {
+        // koszi random user stackoverflow-n
+        output += el.substring(0,3).normalize("NFD").replace(/\p{Diacritic}/gu, "").toLowerCase();
+    });
+    output += `${ev.toString().substring(ev.length-1,ev.length)}@kacsa.hu`;
+    return output;
+}
+
+function compFn(a, b) {
+    return a - b;
+}
+
+function filter(arr, x) {
+    arr = arr.filter(i => i !== x);
+    return arr;
+}
+
+function egyszamjatek(numbers) {
+    let temp = [...numbers];
+    temp.sort(compFn).reverse();
+    while (temp[0] === temp[1] && temp.length > 1) {
+        temp = filter(temp, temp[0]);
+        temp.sort(compFn).reverse();
+    }
+    return temp[0] ? temp[0] : -1;
+}
+
+
+const kacsa = document.getElementById("kacsa");
+const egyszam = document.getElementById("egyszam");
+kacsa.onsubmit = (e)=>{
+    e.preventDefault();
+    let nev = document.getElementById("name").value;
+    let ev = document.getElementById("year").value;
+    document.querySelector("#kacsa>output").innerText = emailCimetGeneral(nev, ev);
+}
+egyszam.onsubmit = (e) => {
+    e.preventDefault();
+    const input = document.getElementById("numbers").value.replace(/[\s;/]/g, ',').replace(/[\[\]]/g, ''); // xdd
+    const numbers = input.split(',').map(Number).filter(num => !isNaN(num));
+    const result = egyszamjatek(numbers);
+    document.querySelector("#egyszam>.output").innerText = result;
+};
